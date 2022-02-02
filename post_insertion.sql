810bc5d8f2442ae5c56715d25c2152e2a7605686
John O'Hara <johnoharaa@gmail.com>
2022-02-01 20:26:01 -0800

    Removed unnecessary file

--- post_insertion.sql
+++
@@ -1,5 +0,0 @@
--- PostInsertion.sql 
--- 
--- A script to define procedures for inserting CSV Post data into the social media database.
-
--- Procedure to get user_id from social_id for inserting categories?
